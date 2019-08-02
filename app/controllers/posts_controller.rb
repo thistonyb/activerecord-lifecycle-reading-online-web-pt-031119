b4fbd5d89965ca86c3119a49c96a68cb427055c5
Tony Baker <this.tonybaker@gmail.com>
2019-08-02 16:57:03 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
 	private
 
-	def post_params(*args)
-		params.require(:post).permit(*args)
+	def post_params
+		params.require(:post).permit(:title, :description)
 	end
 end
